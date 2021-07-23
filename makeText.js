9b0142dfb3081b1b1a44ee516553182046308d4d
Jerry Hsu <Jerry>
2021-07-23 19:53:06 -0400

    Node Markov: Step 1

--- makeText.js
+++ makeText.js
@@ -1,2 +1,8 @@
 /** Command-line tool to generate Markov text. */
+const fs = require("fs");
+let { MarkovMachine } = require("./markov");
+let path = process.argv[2];
 
+let text = fs.readFileSync(path, "utf8");
+machine = new MarkovMachine(text);
+console.log(machine.makeText(100));
--- markov.js
+++ markov.js
@@ -1,6 +1,5 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
 
   /** build markov machine; read in text.*/
@@ -8,7 +7,9 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
-    this.makeChains();
+    
+    this.wordBank = [...new Set(this.words)];
+    this.wordChains = this.makeChains();
   }
 
   /** set markov chains:
@@ -17,13 +18,46 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let wordChains = {};
+    this.wordBank.forEach((word) => {
+      wordChains[word] = this.makeWordChain(word, this.words);
+    });
+    return wordChains;
   }
 
+  makeWordChain(lookupWord, words){
+    let arr = [];
+    words.forEach( (word, idx) => {
+      if (word === lookupWord && idx < words.length-1) {
+        arr.push(words[idx+1]);
+      }
+    });
+    return arr;
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let wordChainArr = [];
+
+    let idx = this.randIdxFromArr(this.wordBank);
+    let currWordBank = this.wordBank;
+    for (let i = 0; i < numWords; i++) {
+      if (currWordBank.length == 0){
+        return wordChainArr;
+      } else {
+        wordChainArr.push(currWordBank[idx]);
+        currWordBank = this.wordChains[currWordBank[idx]];
+        idx = this.randIdxFromArr(currWordBank);
+      }
+    }
+    return wordChainArr;
+  }
+
+  randIdxFromArr(arr){
+    let idx = Math.floor(Math.random() * arr.length);
+    return idx;
   }
 }
+
+module.exports = { MarkovMachine };
